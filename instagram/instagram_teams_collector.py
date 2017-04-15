9770b72614c95492ccb662352760c0c5434d8bbd
Gábor László <laszlogabor91@gmail.com>
2017-04-15 21:22:07 +0200

    Instagram csv generated

--- instagram/instagram_teams_collector.py
+++ instagram/instagram_teams_collector.py
@@ -13,12 +13,21 @@ team_list = teams['team_name']
 #team_list = ['ujpestfc']
 
 
-final_csv = "team_name,insta_id,ista_name,insta_fullname,website,media_count,followed_by,follows\n"
+header = "team_name,insta_id,ista_name,insta_fullname,website,media_count,followed_by,follows\n"
+text = open("teams_insta.csv", "a")
+text.write(header)
+text.close()
 for num in range(0,len(team_list)):
     team = team_list[num]
     
     team_tup = search_team(team)
+    
+    line = team
     for i in team_tup:
-        print i
-        #TODO: csv generate
-    print
+        line += "," + smart_str(i)
+    line += "\n"
+    print line
+    line = smart_str(line)
+    text = open("teams_insta.csv", "a")
+    text.write(line)
+    text.close()
